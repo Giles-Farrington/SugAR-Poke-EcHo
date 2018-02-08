603a53ef22d48f32d1a31366315130b8f1793dc1
An Nguyen <francisnguyen1201@gmail.com>
2018-02-08 11:21:43 -0500

    Set ARCamera focus mode to continuously focus.

---
+++ Assets/Vuforia/Scripts/CameraFocusController.cs
@@ -0,0 +1,40 @@
+﻿/**
+ * Created by An Nguyen 
+ * Date: February 8, 2018
+**/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class CameraFocusController : MonoBehaviour {
+
+	//Set ARCamera focus mode when camera starts
+	void Start ()
+	{
+		Debug.Log ("I'm alive");
+
+		VuforiaARController.Instance.RegisterVuforiaStartedCallback(OnVuforiaStarted); 
+		VuforiaARController.Instance.RegisterOnPauseCallback(OnPaused);
+	
+	}
+
+	//Setting the focus mode to continuously auto focusing
+	private void OnVuforiaStarted()
+	{
+		CameraDevice.Instance.SetFocusMode(
+			CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+	}
+
+	private void OnPaused(bool paused)
+	{
+		if (!paused) // resumed
+		{
+			// Set again autofocus mode when app is resumed
+			CameraDevice.Instance.SetFocusMode(
+				CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+		}
+	}
+
+}
