47f4335ce08699f9a98303b069eef4824de8a0b7
Giles <farringtong@wit.edu>
2018-04-17 14:24:08 -0400

    Rewrote DefaultTrackableEventHandler, so now it should definitely be working on phone builds!

--- Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
+++ Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -1,3 +1,10 @@
+/* /* /*==============================================================================
+Copyright (c) 2017 PTC Inc. All Rights Reserved.
+
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Protected under copyright and other laws.
+==============================================================================*/
 /*==============================================================================
 Copyright (c) 2017 PTC Inc. All Rights Reserved.
 
@@ -17,80 +24,43 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
     #region PRIVATE_MEMBER_VARIABLES
 
     protected TrackableBehaviour mTrackableBehaviour;
+	protected TrackableBehaviour.Status mStatus;
+	protected TrackableBehaviour.Status nStatus;
 	private string imgTargetName = " ";
-	private float[] distance = new float[10];
+	private double distance = 0.5;
+	private double[] prevDist = new double[10];
 	private int count = 0;
-	private float prevDist = 0;
-	private float prevDist2 = 0;
+	private int flag = 0;
 	
 
     #endregion // PRIVATE_MEMBER_VARIABLES
 
     #region UNTIY_MONOBEHAVIOUR_METHODS
 
-	//On start, creates trackable behaviour event handler
     protected virtual void Start()
     {
-		distance[0] = 0;
+		
         mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		mStatus = TrackableBehaviour.Status.DETECTED;
+		nStatus = TrackableBehaviour.Status.NOT_FOUND;
         if (mTrackableBehaviour)
             mTrackableBehaviour.RegisterTrackableEventHandler(this);
     }
-	
-	//On update gets current distance of the current image target and depending on distance of current image target, acts appropiately. 
-	
-	//Essentially 
 	protected virtual void Update(){
-		if(distance[0] == 1){
-			Vector3 delta = Camera.main.transform.position - mTrackableBehaviour.transform.position; //Resets distance to new current image target distance
-			prevDist2 = delta.magnitude;
-			//Debug.Log("IMPORTANT IMPORTANT LOOK AT THIS!!!!!!!: " + prevDist);
-			if(prevDist != prevDist2 ){
-				distance[0] = prevDist2;
-			}
+		Vector3 delta = Camera.main.transform.position - mTrackableBehaviour.transform.position; //Resets distance to new current image target distance
+		distance = delta.magnitude;
+	
+		if(distance < 0.09){
+			distance = 0.5;
 		}
-		Debug.Log("UPDATING!!! DIstance:" + distance[0]);  // 0.00068 < distance < 0.00071  && 0.0048 < distance < 0.0051
-		if(distance[0] <= 0.7){   //Only looks for new distance if current image target distance is less than the maximum distance threshold.
-			if(distance[0] < 0.3) {	//If distance is below 0.1m, most likely a new image target and gets the name, displays the added sugar value and calls OnTrackingFound()function
-				//Debug.Log("DISTANCE STILL LESS THAN 0.2!! " + distance[0] );
-				imgTargetName = mTrackableBehaviour.TrackableName;
-				transform.Find("TeaspoonCounter 2 1").GetComponent<CounterScript>().GetTeaspoonValue(imgTargetName);
-				OnTrackingFound();
-			}
-			else if(distance[0] >= 0.6){	//If distance is greater than maximum threshold (Currently 0.3m), Calls OnTrackingLost() Function
-				Debug.Log("Trackable LOST" + mTrackableBehaviour.TrackableName + " lost");
-				OnTrackingLost();
-				//While utilizing the cloud database scene, uncomment line of code below. When switching back to local database scenes, make sure to re-comment 
-				//GameObject.Find("CloudRecognition").GetComponent<SimpleCloudHandler>().ResetTarget();
-			}
-		}
-		if(distance[0] != 1){
-			Vector3 delta2 = Camera.main.transform.position - mTrackableBehaviour.transform.position; //Resets distance to new current image target distance
-			distance[0] = delta2.magnitude;
-			//Debug.Log("NEW Trackable DISTANCE IS: " + distance[0]);
-		}
-		if (count < 10){
-			distance[count] = distance[0];
-		}
-		if ((distance[9] == distance[0]) && (distance[0] == distance[3]) && (distance[0] == distance[6]) && distance[0] != 0 && distance[0] != 1){
-			Debug.Log("DISTANCE IS SAME, LOSING TRACKING" + distance[0] + distance[1] + distance[2] + distance[9]);
-			Debug.Log("LKAJFSKAHFLKSALKHDSLKJAHDLKAHALKJHDLKADkahskaskadshkjashdkashkadhkjashdkjahdkajhdkjsahkashkahdkahdskashdkjahdkajshdkahdkasjdhaksjhaksjd");
-			OnTrackingLost();
-			count = -1;
-			prevDist = distance[0];
-			distance[0] = 1;
-		}
-		if(count >= 10 && count != -1){
-			count = -1;
-		}
-		else if(count >= 10){
+		if (distance > 0.6){
 			count = 0;
+			OnTrackingLostTwo();
 		}
-		count = count + 1;
-		
-		
-	}
-
+		if (distance < 0.3){
+			OnTrackableStateChanged(nStatus, mStatus);
+		}	
+	} 
 
     #endregion // UNTIY_MONOBEHAVIOUR_METHODS
 
@@ -100,18 +70,58 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
     ///     Implementation of the ITrackableEventHandler function called when the
     ///     tracking state changes.
     /// </summary>
-    public void OnTrackableStateChanged(		//Function is called on a trackable state change
+    public void OnTrackableStateChanged(
         TrackableBehaviour.Status previousStatus,
         TrackableBehaviour.Status newStatus)
     {
+		if(newStatus == TrackableBehaviour.Status.TRACKED && flag == 1){
+			mStatus = TrackableBehaviour.Status.DETECTED;
+			Debug.Log("SHOULD BE RESETTING AFTE RIMAGE SHWONE");
+		}
+        if ((newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) &&
+			distance < 0.3)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+			imgTargetName = mTrackableBehaviour.TrackableName;
+			transform.Find("TeaspoonCounter 2 1").GetComponent<CounterScript>().GetTeaspoonValue(imgTargetName);
+            OnTrackingFound();
+			prevDist[count] = distance;
+			if (count == 9){
+				if(prevDist[0] == prevDist[9]){
+					Debug.Log("FLAG SET OFF!!!");
+					flag = 1;
+					mStatus = TrackableBehaviour.Status.NOT_FOUND;
+				}
+				count = 0;
+			}
+			count = count + 1;
+			
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NOT_FOUND)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+			count = 0;
+            OnTrackingLost();
+        }
+        else
+        {
+            // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+            // Vuforia is starting, but tracking has not been lost or found yet
+            // Call OnTrackingLost() to hide the augmentations
+			count = 0;
+            OnTrackingLost();
+        }
 		
     }
-	
+
     #endregion // PUBLIC_METHODS
 
     #region PRIVATE_METHODS
 
-    protected virtual void OnTrackingFound()	//Called when image target is found
+    protected virtual void OnTrackingFound()
     {
         var rendererComponents = GetComponentsInChildren<Renderer>(true);
         var colliderComponents = GetComponentsInChildren<Collider>(true);
@@ -131,7 +141,7 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
     }
 
 
-    protected virtual void OnTrackingLost()		//Called when image target is lost
+    protected virtual void OnTrackingLost()
     {
         var rendererComponents = GetComponentsInChildren<Renderer>(true);
         var colliderComponents = GetComponentsInChildren<Collider>(true);
@@ -150,8 +160,23 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
             component.enabled = false;
     }
 	
-	
-	
+	protected virtual void OnTrackingLostTwo(){
+		var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Disable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = false;
+
+        // Disable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = false;
+
+        // Disable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = false;
+	}
 
     #endregion // PRIVATE_METHODS
 }
