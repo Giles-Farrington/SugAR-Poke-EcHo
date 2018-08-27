9e37089141f8758df663f53932ea54a81c36d125
Matthew Clinton <mfclinton0@gmail.com>
2018-08-27 12:58:13 -0400

    Added Functioning Buttons

---
+++ Assets/OpenWebpage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenWebpage : MonoBehaviour {
+
+	public void OpenPage(string page)
+    {
+        Application.OpenURL(page);
+    }
+}
